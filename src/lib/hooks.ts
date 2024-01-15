7837cd208334055c320e2b98924bd9388aad99ee
abhinav <abhinav@hood.live>
2024-01-15 16:18:36 +0530

    add active state and result count

--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -40,7 +40,7 @@ export function useActiveId() {
 export function useJobItems(searchText: string) {
   const [jobItems, setJobItems] = useState<jobItem[]>([]);
   const [isLoading, setIsLoading] = useState(false);
-
+  const totalNumberOfResults = jobItems.length
   const jobItemsSliced = jobItems.slice(0, 7);
 
   useEffect(() => {
@@ -54,5 +54,5 @@ export function useJobItems(searchText: string) {
     };
     fetchData();
   }, [searchText]);
-  return [jobItemsSliced, isLoading] as const;
+  return [jobItemsSliced, isLoading,totalNumberOfResults] as const;
 }
